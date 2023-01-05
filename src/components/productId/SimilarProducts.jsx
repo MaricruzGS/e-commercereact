19c76eaf9c6d6ffe076b7e2475ecabeaf67538a6
MaricruzGS <farma.alta.doc@gmail.com>
2023-01-04 20:35:40 -0600

    second commit

--- src/components/productId/SimilarProducts.jsx
+++ src/components/productId/SimilarProducts.jsx
@@ -1,22 +1,31 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../home/ProductCard'
 
 const SimilarProducts = ({categories, product}) => {
-  console.log(categories, product)
-   
+ 
+   const [productsByCategory, setProductsByCategory] = useState([])
+
+
     useEffect(() => {
-      const category = categories.filter(category => category.name === product.category)
-      console.log(category)
-      const URL = "https://e-commerce-api.academlo.tech/api/v1/products?category=2"
-      axios.get(URL)
-      .then(res => console.log(res.data))
-      .catch( err => console.log(err))
-    }, [])
+      if(categories && product){
+        const category = categories.filter(category => category.name === product.category)
+        const URL = `https://e-commerce-api.academlo.tech/api/v1/products?category=${category[0].id}`
+        axios.get(URL)
+          .then(res => {
+            const productsFilter = res.data.data.products.filter(productCategory => productCategory.id !== product.id)
+            setProductsByCategory(productsFilter)
+          })
+          .catch(err => console.log(err))
+      }
+    }, [categories, product])
     
 
   return (
     <section>
-        
+        {
+          productsByCategory.map(productByCategory => <ProductCard product={productByCategory} key={productByCategory.id}/>)
+        }
     </section>
   )
 }
